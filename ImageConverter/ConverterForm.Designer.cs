e775b056a0acd08e208d171beab6bee31719e06e
Niladri28dutta <nild2141@gmail.com>
2017-12-24 21:11:17 +0530

    First commit

---
+++ ImageConverter/ConverterForm.Designer.cs
@@ -0,0 +1,365 @@
+﻿namespace ImageConverter
+{
+    partial class ConverterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ConverterForm));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.alttext = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.imgtob64 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            this.imgtob64.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "jpg";
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.InitialDirectory = "c:\\";
+            this.openFileDialog1.RestoreDirectory = true;
+            this.openFileDialog1.Title = "Browse image files";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(7, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(241, 17);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Click the button to select image:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(289, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Select image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(7, 222);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(279, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Do you want to Generate <img> tag ?";
+            this.label4.Visible = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.radioButton4);
+            this.panel2.Controls.Add(this.radioButton3);
+            this.panel2.Location = new System.Drawing.Point(292, 222);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 24);
+            this.panel2.TabIndex = 4;
+            this.panel2.Visible = false;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(53, 0);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(39, 17);
+            this.radioButton4.TabIndex = 1;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "No";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(4, 0);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(43, 17);
+            this.radioButton3.TabIndex = 0;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Yes";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(7, 266);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(270, 17);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Alternative text for image(optional) :";
+            this.label5.Visible = false;
+            // 
+            // alttext
+            // 
+            this.alttext.Location = new System.Drawing.Point(292, 266);
+            this.alttext.Name = "alttext";
+            this.alttext.Size = new System.Drawing.Size(200, 20);
+            this.alttext.TabIndex = 6;
+            this.alttext.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(199, 310);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Convert to Base64 string";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(292, 352);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 94);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(296, 462);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(200, 94);
+            this.textBox2.TabIndex = 11;
+            this.textBox2.Visible = false;
+            // 
+            // imgtob64
+            // 
+            this.imgtob64.Controls.Add(this.label7);
+            this.imgtob64.Controls.Add(this.label6);
+            this.imgtob64.Controls.Add(this.button4);
+            this.imgtob64.Controls.Add(this.button3);
+            this.imgtob64.Controls.Add(this.label2);
+            this.imgtob64.Controls.Add(this.label1);
+            this.imgtob64.Controls.Add(this.pictureBox2);
+            this.imgtob64.Controls.Add(this.textBox2);
+            this.imgtob64.Controls.Add(this.textBox1);
+            this.imgtob64.Controls.Add(this.button2);
+            this.imgtob64.Controls.Add(this.alttext);
+            this.imgtob64.Controls.Add(this.label5);
+            this.imgtob64.Controls.Add(this.panel2);
+            this.imgtob64.Controls.Add(this.label4);
+            this.imgtob64.Controls.Add(this.button1);
+            this.imgtob64.Controls.Add(this.label3);
+            this.imgtob64.Location = new System.Drawing.Point(12, 81);
+            this.imgtob64.Name = "imgtob64";
+            this.imgtob64.Size = new System.Drawing.Size(533, 570);
+            this.imgtob64.TabIndex = 3;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(7, 462);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(166, 17);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Generated <img> tag:";
+            this.label7.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(7, 355);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(256, 17);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Generated Base64 string output : ";
+            this.label6.Visible = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(10, 510);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(168, 23);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "Click here to copy html source";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(10, 399);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(168, 23);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Click here to copy base64 string";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(349, 315);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 15);
+            this.label2.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 17);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Preview Thumbnail:";
+            this.label1.Visible = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(291, 91);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(105, 109);
+            this.pictureBox2.TabIndex = 12;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(6, 470);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(0, 0);
+            this.flowLayoutPanel1.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ImageConverter.Properties.Resources.Cool_Text_189872756396443;
+            this.pictureBox1.Location = new System.Drawing.Point(130, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(339, 63);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(305, 48);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(150, 17);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "by Niladri sekhar dutta";
+            // 
+            // ConverterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 662);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.imgtob64);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(600, 700);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(600, 700);
+            this.Name = "ConverterForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Image Converter tool V1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.imgtob64.ResumeLayout(false);
+            this.imgtob64.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox alttext;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Panel imgtob64;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ ImageConverter/ConverterForm.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ImageConverter
+{
+    public partial class ConverterForm : Form
+    {
+        SaveFileDialog save = new SaveFileDialog();
+        public ConverterForm()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            deleteFiles();
+        }
+
+        
+
+        private void deleteFiles()
+        {
+            string[] filenames = null;
+            filenames = Directory.GetFiles(Path.GetDirectoryName(getTempfilepath()));
+            foreach (string filename in filenames)
+            {
+                File.Delete(filename);
+            }
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string inputfilepath = string.Empty;
+            textBox1.Text = "";
+            textBox2.Text = "";
+            openFileDialog1.Filter = "Images (*.BMP;*.JPG;*.GIF,*.PNG,*.TIFF)|*.BMP;*.JPG;*.GIF;*.PNG;*.TIFF|" + "All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+            openFileDialog1.ShowHelp = true;
+            openFileDialog1.FileName = "image.jpg";
+            DialogResult result = openFileDialog1.ShowDialog();
+            if(result == DialogResult.OK)
+            {
+                inputfilepath = openFileDialog1.FileName;
+                File.Copy(inputfilepath,getTempfilepath(), true);
+                showThumbnail(getTempfilepath());
+                label1.Visible = true;
+                pictureBox2.Visible = true;
+                button2.Visible = true;
+                label4.Visible = true;
+                panel2.Visible = true;
+
+                
+            }
+        }
+
+        private string getTempfilepath()
+        {
+            
+            string currentdir, currentdirpath, fileextension = string.Empty;
+            string temppath = string.Empty;
+            
+            fileextension = Path.GetExtension(openFileDialog1.SafeFileName);
+            
+            currentdirpath = AppDomain.CurrentDomain.BaseDirectory;
+            currentdir = Path.GetDirectoryName(currentdirpath);
+            if (currentdir.Contains("\\bin\\Debug"))
+            {
+                temppath = currentdir.Replace("\\bin\\Debug", "\\Tempimages\\");
+            }
+
+            if (currentdir.Contains("\\bin\\Release"))
+            {
+                temppath = currentdir.Replace("\\bin\\Release", "\\Tempimages\\");
+            }
+
+            temppath = temppath + "tempimage" + fileextension;
+
+            return temppath;
+        }
+
+        private void showThumbnail(string filepath)
+        {
+            pictureBox2.Height = 100;
+            pictureBox2.Width = 100;
+            try
+            {
+                Image.GetThumbnailImageAbort callbackfunction = new Image.GetThumbnailImageAbort(ThumbnailCallback);
+                Bitmap bmp = new Bitmap(filepath);
+                Image thumbnail = bmp.GetThumbnailImage(96, 96, callbackfunction, IntPtr.Zero);
+                pictureBox2.Image = thumbnail;
+                 
+            }
+            catch(Exception err)
+            {
+                MessageBox.Show("Error :" + err.Message);
+            }
+         }
+
+        
+      public bool ThumbnailCallback()
+        {
+            return false;
+        }
+
+      private void radioButton3_CheckedChanged(object sender, EventArgs e)
+      {
+          if(radioButton3.Checked)
+          {
+              label5.Visible = true;
+              alttext.Visible = true;
+              
+              textBox1.Text = "";
+              label6.Visible = false;
+              textBox1.Visible = false;
+              button3.Visible = false;
+              button4.Visible = false;
+             
+              label7.Visible = false;
+
+          }
+      }
+
+      private void radioButton4_CheckedChanged(object sender, EventArgs e)
+      {
+          if (radioButton4.Checked)
+          {
+              label5.Visible =  false;
+              alttext.Visible = false;
+              alttext.Text = "";
+             
+              textBox1.Text = "";
+              label6.Visible = false;
+              textBox1.Visible = false;
+              label7.Visible = false;
+              textBox2.Visible = false;
+             
+              textBox2.Text = "";
+              button3.Visible = false;
+              button4.Visible = false;
+          }
+      }
+
+      private void button2_Click(object sender, EventArgs e)
+      {
+          string base64string = null;
+          
+          string extension,alternatetext,htmlstring = string.Empty;
+          
+          if(!string.IsNullOrEmpty(openFileDialog1.SafeFileName))
+          {
+              using (System.Drawing.Image img = System.Drawing.Image.FromFile(getTempfilepath()))
+              {
+                  using (MemoryStream mo = new MemoryStream())
+                  {
+                      img.Save(mo, img.RawFormat);
+                      byte[] imgbytes = mo.ToArray();
+                      base64string = Convert.ToBase64String(imgbytes);
+                      textBox1.Text = base64string;
+                  }
+                  
+              }
+              label6.Visible = true;
+              button3.Visible = true;
+              textBox1.Visible = true;
+              if (radioButton3.Checked)
+              {
+                 if (!string.IsNullOrEmpty(alttext.Text.ToString()))
+                  {
+                      alternatetext = alttext.Text.ToString();
+                  }
+                  else
+                  {
+                      alternatetext = Path.GetFileNameWithoutExtension(openFileDialog1.SafeFileName);
+                  }
+
+                  extension = Path.GetExtension(openFileDialog1.SafeFileName);
+                  extension = extension.Trim('.');
+                  htmlstring = "<img src=\"data:image/" + extension + ";base64," + base64string + "\"" + " alt=\"" + alternatetext + "\" >";
+                  textBox2.Text = htmlstring;
+                  
+                  label7.Visible = true;
+                  button4.Visible = true;
+                  textBox2.Visible = true;
+                  
+              }
+              
+          }
+          else
+          {
+              label2.Text = "Please select a file to upload";
+              return;
+          }
+      }
+
+       private void copyToClipboard(string text)
+      {
+          Clipboard.SetText(text);
+          MessageBox.Show("text copied", "Success!");
+      }
+
+       private void button3_Click(object sender, EventArgs e)
+       {
+           copyToClipboard(textBox1.Text);
+       }
+
+       private void button4_Click(object sender, EventArgs e)
+       {
+           copyToClipboard(textBox2.Text);
+       }
+
+      }
+}
